99392df9298ae4e5fef9856e05df27bf6ac0aa26
gphani333 <81351497+gphani333@users.noreply.github.com>
2021-04-29 17:35:31 +0530

    5 and 6

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Odd Nos 1..3..!" );
-	System.out.println( "Even Nos 2..4..!" );
+        System.out.println( "Odd Nos 1..3..5..!" );
+	System.out.println( "Even Nos 2..4..6..!" );
     }
 }
