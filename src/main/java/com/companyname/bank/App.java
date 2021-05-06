40c72e266cc302a9e933f15f979a1548fecf678f
gphani333 <81351497+gphani333@users.noreply.github.com>
2021-05-06 17:57:39 +0530

    With name

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Phani..!" );
+        System.out.println( "G.Phani..!" );
 	System.out.println( "Chaitanya..!" );
     }
 }
